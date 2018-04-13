627b1ccdc92338248aaaf0b02c2e07324fa7a506
uni51 <uni51@users.noreply.github.com>
2018-04-13 12:17:47 +0900

    コールバックの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,27 @@
 class User < ApplicationRecord
+  # before_create :ensure_has_name
+
+  # after_initialize do |user|
+  #   puts "オブジェクトを生成しました！"
+  #   user.name = "Taro"
+  # end
+
+  # before_create do
+  #   if name.blank?
+  #     self.name = 'Jiro'
+  #   end
+  # end
+
+  before_save do
+    puts '１番目に呼ばれたコールバック'
+  end
+
+  before_save do
+    puts '２番目に呼ばれたコールバック'
+  end
+
+  # private
+  # def ensure_has_name
+  #   self.name = 'Taro' if name.blank?
+  # end
 end
